f25d015a82b5af48082be38f28b4dd1f54bc38eb
MirayTopac <miray9013@gmail.com>
2024-06-03 21:29:05 +0300

    Add files via upload

---
+++ VarsayilanParametreliMethod/Sinif.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarsayilanParametreliMethod
+{
+    class Sinif
+    {
+        public void Selamla(string ad = " Miray")
+        {
+            Console.WriteLine("Merhaba"+ad);
+        }
+    }
+}
