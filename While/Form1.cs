4d64cd7a6524114c231741ced9690b7e1704df04
MirayTopac <miray9013@gmail.com>
2024-06-03 21:30:36 +0300

    Add files via upload

---
+++ While/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say = 0;
+            do
+            {
+                MessageBox.Show("Döngü çalışıyor.");
+                if (say == 5)
+                {
+                    MessageBox.Show("Döngü durdu.");
+                    break;
+                }
+                say++;
+            } while (say < 10);
+        }
+    }
+}
