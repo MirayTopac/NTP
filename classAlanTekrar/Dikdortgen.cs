9c13eb05d47a6849f7a5f8029ee78c713b98e2b7
MirayTopac <miray9013@gmail.com>
2024-06-03 20:54:53 +0300

    Add files via upload

---
+++ classAlanTekrar/Dikdortgen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAlanTekrar
+{
+    class Dikdortgen
+    {
+        private int a, b;
+        public Dikdortgen(int a, int b)//parametreli metot
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
