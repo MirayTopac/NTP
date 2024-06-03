a0cea1f44712875e2da07820331c1789e2da0eb9
MirayTopac <miray9013@gmail.com>
2024-06-03 21:10:28 +0300

    Add files via upload

---
+++ Field_uyglaması/Ucgen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_uyglaması
+{
+    class Ucgen
+    {
+        public int a;
+        public int b;
+        public int c;
+    }
+}
