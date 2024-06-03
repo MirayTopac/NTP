e20c417ff4d0998cb6fa2e5c663924d000cc0738
MirayTopac <miray9013@gmail.com>
2024-06-03 20:57:43 +0300

    Add files via upload

---
+++ ClassUygulama2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassUygulama2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(5, 6);
+            Console.WriteLine("Dikdörtgenin Alanı: {0}",dikdortgen.AlanHesapla());
+            Console.WriteLine("Dikdörtgenin Çevresi: {0}", dikdortgen.CevreHesapla());
+            Console.ReadLine();
+        }
+    }
+}
