b8f6b26558b67065eb9115902173a57fff2ea4ea
MirayTopac <miray9013@gmail.com>
2024-06-03 21:19:22 +0300

    Add files via upload

---
+++ metot_buyuk_sayi_bulma/BuyukSayiBul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot_buyuk_sayi_bulma
+{
+    class BuyukSayiBul
+    {
+        private int a, b, c;
+        public int SayiBulucu(int sayi1, int sayi2)
+        {
+            int sonuc;
+            if(sayi1<sayi2)
+            {
+                sonuc = sayi2;
+            }
+            else
+            {
+                sonuc = sayi1;
+            }
+            return sonuc;
+        }
+    }
+}
