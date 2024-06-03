c5f283de4a7b214f1f4ff7f00915d95b1ee485c2
MirayTopac <miray9013@gmail.com>
2024-06-03 21:15:08 +0300

    Add files via upload

---
+++ InterfaceHayvanUygulamasi/Kedi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHayvanUygulamasi
+{
+    class Kedi : IHayvan,IBeslen
+    {
+        public void Beslen()
+        {
+            Console.WriteLine("Kedi süt içti");
+        }
+
+        public void SesCikar()
+        {
+            Console.WriteLine("Kedi : miav miav");
+        }
+    }
+}
