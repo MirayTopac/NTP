81fd42d4d73b3edf68f97f5b3dd115dcc1852e39
MirayTopac <miray9013@gmail.com>
2024-06-03 21:16:24 +0300

    Add files via upload

---
+++ InterfaceUyglamasi/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUyglamasi
+{
+    class Manager : IWorker,ISalary,IEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Yonetici yemek yer");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Yonetici maaş alır.");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Yonetici çalışıyor.");
+        }
+    }
+}
---
+++ InterfaceUyglamasi/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUyglamasi
+{
+    class Worker : IWorker,ISalary,IEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Worker yemek yer.");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Worker maaş alır.");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Worker çalışıyor.");
+        }
+    }
+}
